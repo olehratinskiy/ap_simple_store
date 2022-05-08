34cd1da46303bc7c9c5091d55134c2a743de7d30
olehratinskiy <oleh.ratinskyi.knm.2020@lpnu.ua>
2022-05-08 18:59:51 +0300

    add item_quantity and storage_quantity fields

--- models.py
+++ models.py
@@ -14,9 +14,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, scoped_session, relationship, backref
 import sys
 
-#sys.path.append("/home/yuliamarkiv/ap_virtualenv_6_variant")
-
-DB_URI = os.getenv("DB_URI", "mysql://root:123abc!!!@127.0.0.1:3306/store")
+DB_URI = os.getenv("DB_URI", "mysql://root:A2452756b@127.0.0.1:3306/store")
 
 engine = create_engine(DB_URI)
 SessionFactory = sessionmaker(bind=engine)
@@ -25,35 +23,35 @@ Session = scoped_session(SessionFactory)
 
 BaseModel = declarative_base()
 
-class User(BaseModel):
- __tablename__ = "user"
 
- user_id = Column(Integer, primary_key=True)
- username = Column(String(45), nullable=False)
- first_name = Column(String(45), nullable=False)
- last_name = Column(String(45), nullable=False)
- email = Column(String(45), nullable=False)
- password = Column(String(2000), nullable=False)
+class User(BaseModel):
+    __tablename__ = "user"
+    user_id = Column(Integer, primary_key=True)
+    username = Column(String(45), nullable=False)
+    first_name = Column(String(45), nullable=False)
+    last_name = Column(String(45), nullable=False)
+    email = Column(String(45), nullable=False)
+    password = Column(String(2000), nullable=False)
+
 
 class Item(BaseModel):
- __tablename__ = "item"
-
- item_id = Column(Integer, primary_key=True)
- name = Column(String(45), nullable=False)
- quantity = Column(Integer, nullable=False)
- price = Column(Integer, nullable=False)
- status = Column(Enum("sold out", "available"))
+    __tablename__ = "item"
+    item_id = Column(Integer, primary_key=True)
+    name = Column(String(45), nullable=False)
+    storage_quantity = Column(Integer, nullable=False)
+    price = Column(Integer, nullable=False)
+    status = Column(Enum("sold out", "available"))
 
 
 class Orders(BaseModel):
- __tablename__ = "orders"
-
- order_id = Column(Integer, primary_key=True)
- user_id = Column(Integer, ForeignKey('user.user_id'))
- user = relationship("User", backref=backref("orders", uselist=False))
- item_id = Column(Integer, ForeignKey('item.item_id'))
- item = relationship("Item", backref=backref("orders", uselist=False))
- price = Column(Integer)
+    __tablename__ = "orders"
+    order_id = Column(Integer, primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.user_id'))
+    user = relationship("User", backref=backref("orders", uselist=False))
+    item_id = Column(Integer, ForeignKey('item.item_id'))
+    item = relationship("Item", backref=backref("orders", uselist=False))
+    item_quantity = Column(Integer, nullable=False)
+    price = Column(Integer)
 
 
 
