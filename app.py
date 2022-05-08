16dd12a74c7cf0ea3873ddb5e9f110b0aa38cfdf
olehratinskiy <oleh.ratinskyi.knm.2020@lpnu.ua>
2022-05-08 19:01:07 +0300

    change methods in app.py

--- app.py
+++ app.py
@@ -1,10 +1,7 @@
 from flask import Flask, request, jsonify
-# from flask_jwt import JWT, jwt_required, current_identity
 from flask_jwt_extended import create_access_token, jwt_required, get_jwt_identity, JWTManager
 from wsgiref.simple_server import make_server
-# from werkzeug.security import safe_str_cmp
 from greeting import api
-# from sqlalchemy.exc import NoResultFound
 from flask_bcrypt import Bcrypt
 from schema import *
 from models import Session
@@ -14,16 +11,10 @@ app = Flask(__name__)
 bcrypt = Bcrypt(app)
 jwt = JWTManager(app)
 app.config["JWT_SECRET_KEY"] = "super-secret"
-app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=1)
-
-if session.query(User).filter_by(username='admin').first() is None:
-    admin = User(username='admin', first_name='Ake', last_name='Harwood', email='email1@gmail.com',
-                 password=bcrypt.generate_password_hash('adminpassword').decode('utf-8'))
-    session.add(admin)
-    session.commit()
+app.config["JWT_ACCESS_TOKEN_EXPIRES"] = timedelta(hours=2)
 
 
-@app.route('/api/v1/item',methods =["POST"])
+@app.route('/item', methods=["POST"])
 @jwt_required()
 def add_item():
     current_identity_username = get_jwt_identity()
@@ -31,7 +22,7 @@ def add_item():
         return 'Access is denied', 403
 
     data = request.get_json()
-    new_item = Item(name=data['name'], quantity=data['quantity'], price=data['price'], status=data['status'])
+    new_item = Item(name=data['name'], storage_quantity=data['storage_quantity'], price=data['price'], status=data['status'])
     quantity = session.query(Item.item_id).count()
     if quantity >= 8:
         return 'Exceed number of items', 400
@@ -43,20 +34,20 @@ def add_item():
         return 'Invalid input', 405
 
 
-@app.route('/api/v1/item/<id>', methods = ["GET"])
-def get_item_by_id(id):
-    item = session.query(Item).filter_by(item_id = int(id)).first()
+@app.route('/item/<item_id>', methods=["POST"])
+def get_item_by_id(item_id):
+    item = session.query(Item).filter_by(item_id=int(item_id)).first()
     if item is not None:
         return ItemSchema().dump(item), 200
-    elif not id:
-        return 'Invalid id', 400
+    elif not item_id:
+        return 'Invalid item id', 400
     else:
         return 'Item was not found', 404
 
 
-@app.route('/api/v1/item/<id>',methods = ["PUT"])
+@app.route('/item/<item_id>', methods=["PUT"])
 @jwt_required()
-def put_item_by_id(id):
+def put_item_by_id(item_id):
     current_identity_username = get_jwt_identity()
     if current_identity_username != 'admin':
         return 'Access is denied', 403
@@ -69,7 +60,7 @@ def put_item_by_id(id):
         _quantity = item['quantity']
         _price = item['price']
         _status = item['status']
-        item_1 = session.query(Item).filter_by(item_id=int(id)).first()
+        item_1 = session.query(Item).filter_by(item_id=int(item_id)).first()
         if item_1 is not None:
             item_1.name = _name
             item_1.quantity = _quantity
@@ -81,16 +72,16 @@ def put_item_by_id(id):
             return 'Item was not found', 400
 
 
-@app.route('/api/v1/item/<id>', methods = ["DELETE"])
+@app.route('/item/<item_id>', methods=["DELETE"])
 @jwt_required()
-def delete_item(id):
+def delete_item(item_id):
     current_identity_username = get_jwt_identity()
     if current_identity_username != 'admin':
         return 'Access is denied', 403
 
-    if not id:
+    if not item_id:
         return 'Invalid input', 400
-    item = session.query(Item).filter_by(item_id=int(id)).first()
+    item = session.query(Item).filter_by(item_id=int(item_id)).first()
     if item is None:
         return 'Item not found', 404
     else:
@@ -103,9 +94,9 @@ def delete_item(id):
             return 'Item can not be deleted', 400
 
 
-@app.route('/api/v1/store',methods =["POST"])
+@app.route('/order', methods=["POST"])
 @jwt_required()
-def add_store():
+def add_order():
     admin_id = -1
     admin_data = session.query(User).filter_by(username='admin').first()
     if admin_data:
@@ -124,94 +115,103 @@ def add_store():
         if user.user_id != int(data['user_id']):
             return 'Access is denied', 403
 
-    new_store = Orders(user_id=data['user_id'], item_id=data['item_id'], price=data['price'])
+    new_order = Orders(user_id=data['user_id'], item_id=data['item_id'], item_quantity=data['item_quantity'], price=data['price'])
     if not session.query(User).filter_by(user_id=data['user_id']).first():
         return 'Invalid input', 400
     elif not session.query(Item).filter_by(item_id=data['item_id']).first():
         return 'Invalid input', 400
-    elif session.query(Orders).filter_by(item_id=data['item_id']).first():
+    elif session.query(Item).filter_by(item_id=data['item_id']).first().status == 'sold out':
         return 'Can not be bought', 400
     else:
-        session.add(new_store)
+        session.add(new_order)
         item = session.query(Item).filter_by(item_id=data['item_id']).first()
-        item.status = 'sold out'
+        item.storage_quantity -= int(new_order.item_quantity)
+        if item.storage_quantity == 0:
+            item.status = 'sold out'
         session.commit()
         return 'Successful operation', 200
 
 
-@app.route('/api/v1/store/<id>', methods = ["GET"])
+@app.route('/orders', methods=["POST"])
 @jwt_required()
-def get_store_by_id(id):
-    store = session.query(Orders).filter_by(order_id = int(id)).first()
+def get_orders():
     current_identity_username = get_jwt_identity()
     user = session.query(User).filter_by(username=current_identity_username).first()
-    if store is not None:
-        if user is not None:
-            if user.user_id != store.user_id:
-                return 'Access is denied', 403
-            return OrdersSchema().dump(store), 200
+    if user is not None:
+        orders = session.query(Orders).filter_by(user_id=user.user_id).all()
+        if orders:
+            schema = OrdersSchema(many=True)
+            return jsonify(schema.dump(orders))
         else:
-            return 'User with this token does not exist', 404
-    elif id not in session.query(Orders.order_id).first():
-        return 'Invalid input', 400
+            return 'User does not have any orders', 404
     else:
-        return 'Order was not found', 404
+        return 'User with this token does not exist', 404
 
 
-@app.route('/api/v1/store/<id>', methods = ["DELETE"])
+@app.route('/order/<order_id>', methods=["DELETE"])
 @jwt_required()
-def delete_store(id):
+def delete_order(order_id):
     current_identity_username = get_jwt_identity()
     user = session.query(User).filter_by(username=current_identity_username).first()
 
-    if not id:
+    if not order_id:
         return 'Invalid input', 400
-    store = session.query(Orders).filter_by(order_id=int(id)).first()
-    if store is None:
+    order = session.query(Orders).filter_by(order_id=int(order_id)).first()
+    if order is None:
         return 'Invalid input', 400
-    elif store.user_id != user.user_id:
+    elif order.user_id != user.user_id:
         return 'Access is denied', 403
     else:
-        item = session.query(Item).filter_by(item_id=store.item_id).first()
-        item.status = 'available'
-        session.delete(store)
+        item = session.query(Item).filter_by(item_id=order.item_id).first()
+        if item.status == 'sold out':
+            item.status = 'available'
+        item.storage_quantity += int(order.item_quantity)
+        session.delete(order)
         session.commit()
         return 'Successful operation', 200
 
 
-@app.route('/api/v1/user', methods = ['POST'])
+@app.route('/user', methods=['POST'])
 def add_user():
     data = request.get_json()
     if data is None:
-        return "Invalid username/password supplied",400
-    new_user = User(username = data['username'],first_name = data['first_name'],last_name = data['last_name'], email = data['email'],
-    password = bcrypt.generate_password_hash(data['password']).decode('utf-8'))
+        return "Invalid username/password supplied", 400
+    new_user = User(username=data['username'], first_name=data['first_name'], last_name=data['last_name'],
+                    email=data['email'], password=bcrypt.generate_password_hash(data['password']).decode('utf-8'))
     if session.query(User).filter_by(username=data['username']).first() is None:
         session.add(new_user)
         session.commit()
-        return jsonify ({'message': 'user created'})
+        return jsonify({'message': 'user created'})
     else:
-        return "Username is already in use",404
+        return "Username is already in use", 404
 
 
-@app.route('/api/v1/user/login', methods = ["GET"])
+@app.route('/user/login', methods=["POST"])
 def login_user():
-    auth = request.authorization
-    if not auth or not auth.username or not auth.password:
-        return 'Wrong data provided', 401
+    data = request.get_json()
 
-    user = session.query(User).filter_by(username=auth.username).first()
+    if not data or 'username' not in data or 'password' not in data:
+        return {'message': 'Wrong input data provided'}, 400
 
-    if user is not None:
-        if not bcrypt.check_password_hash(user.password, auth.password):
-            return 'Invalid username or password supplied', 400
-        access_token = create_access_token(identity=user.username)
-        return jsonify({'token': access_token})
-    else:
-        return 'User not found', 404
+    if data['username'] == 'admin' and data['password'] == 'adminpassword':
+        access_token = create_access_token(identity='admin')
+        return {'token': access_token}
+
+    user = session.query(User).filter_by(username=data['username']).first()
+
+    if user is None:
+        return {'message': 'User not found'}, 404
+
+    if not bcrypt.check_password_hash(user.password, data['password']):
+        return {'message': 'Invalid username or password provided'}, 400
+
+    access_token = create_access_token(identity=user.username)
+    session.close()
+
+    return {'token': access_token}
 
 
-@app.route('/api/v1/user/<username>', methods = ['GET'])
+@app.route('/user/<username>', methods=['POST'])
 @jwt_required()
 def get_user(username):
     current_identity_username = get_jwt_identity()
@@ -225,7 +225,7 @@ def get_user(username):
     return UserSchema().dump(user), 200
 
 
-@app.route('/api/v1/user/<username>', methods = ["PUT"])
+@app.route('/user/<username>', methods=["PUT"])
 @jwt_required()
 def put_user_by_username(username):
     current_identity_username = get_jwt_identity()
@@ -255,7 +255,7 @@ def put_user_by_username(username):
         return jsonify({'token': access_token})
 
 
-@app.route('/api/v1/user/<username>', methods = ["DELETE"])
+@app.route('/user/<username>', methods=["DELETE"])
 @jwt_required()
 def delete_user(username):
     if not username:
@@ -265,7 +265,7 @@ def delete_user(username):
         return 'User not found', 404
     else:
         current_identity_username = get_jwt_identity()
-        user_in_order = session.query(Orders).filter_by(user_id = user.user_id).first()
+        user_in_order = session.query(Orders).filter_by(user_id=user.user_id).first()
 
         if user is not None and user_in_order is None:
             if current_identity_username != user.username:
@@ -278,5 +278,5 @@ def delete_user(username):
 
 
 with make_server('', 5000, app) as server:
-    app.register_blueprint(api, url_prefix="/api/v1")
+    app.register_blueprint(api, url_prefix="/")
     # server.serve_forever()
